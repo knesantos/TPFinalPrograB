e5e411eea2f3f3e45be50bf695552d12eef88f04
CatalinaArcidiacono <cataarcidiacono04@gmail.com>
2023-07-06 23:19:02 -0300

    develop Auto.java Neumatico.java Hard.java Medium.java Soft.java Wet.java ModoManejo.java

---
+++ src/Core/Entities/Wet.java
@@ -0,0 +1,9 @@
+package Core.Entities;
+
+public class Wet extends Neumatico{
+
+	public Wet(int desgaste, int durabilidad, int consumo, int velocidad, int agarre) {
+		super(desgaste, durabilidad, consumo, velocidad, agarre);
+	}
+
+}
