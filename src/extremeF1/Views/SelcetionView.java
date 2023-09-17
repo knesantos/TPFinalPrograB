bd2fa7a1c77c1f4acd0d4f8e64fa6fa7a9561a37
GonzaloIbarguren <ibargurengonzalo03@gmail.com>
2023-09-16 23:08:10 -0300

    Modificacion de la clase SelectionView

--- src/extremeF1/Views/SelcetionView.java
+++ src/extremeF1/Views/SelcetionView.java
@@ -6,19 +6,19 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-
 import Core.Entities.Piloto;
-
 import java.awt.Color;
 import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SelcetionView extends JFrame{
+public class SelcetionView extends JFrame {
 	
 	private JPanel panel;
 	private JButton boton1,boton2;
 	private JLabel titulo,nombre,nomabv,imagen,presupuesto,defensa,sobrepaso,clasificacion,cantCarrerasGanadas,cantPolePosition,cantCampeonatos,cantParticipaciones,cuidadoNeumaticos,largada;
 	private List<Piloto> Pilotos = new ArrayList<>();
-	
+	private int i=0;
 	
  public SelcetionView(List<Piloto> listpilotos) {
 	 Pilotos = listpilotos;
@@ -43,7 +43,54 @@ public class SelcetionView extends JFrame{
 	 	boton2.setBounds(100, 50, 50, 50);
 	 	boton1.setText("<");
 	 	boton2.setText(">");
-	
+	 	boton2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (i == Pilotos.size()-1) {
+					boton2.setEnabled(false);
+				}else {
+					i++;
+					boton1.setEnabled(true);
+				nombre.setText("Nombre: "+ Pilotos.get(i).getnombre());
+				nomabv.setText(Pilotos.get(i).getnombreAbrev());
+				presupuesto.setText("Presupuesto: " + Pilotos.get(i).getpresupuesto());
+				defensa.setText("Defensa: "+ Pilotos.get(i).getdefensa());
+				sobrepaso.setText("Sobrepaso: "+ Pilotos.get(i).getsobrepaso());
+				clasificacion.setText("Clasificacion: "+Pilotos.get(i).getclasificacion());
+				cantCarrerasGanadas.setText("Cantiddad de carreras ganadas: "+ Pilotos.get(i).getcantCarrerasGanadas());
+				cantParticipaciones.setText("Cantiddad de parcitipaciones: "+ Pilotos.get(i).getcantParticipaciones());
+				cuidadoNeumaticos.setText("Cuidado de neumaticos: "+ Pilotos.get(i).getcuidadoNeumaticos());
+				largada.setText("Largada: "+ Pilotos.get(i).getlargada());
+				}
+			}
+	 		
+	 	});
+	 	boton1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (i == 0) {
+					boton1.setEnabled(false);
+				}else {
+					i--;
+					boton2.setEnabled(true);
+				nombre.setText("Nombre: "+ Pilotos.get(i).getnombre());
+				nomabv.setText(Pilotos.get(i).getnombreAbrev());
+				presupuesto.setText("Presupuesto: " + Pilotos.get(i).getpresupuesto());
+				defensa.setText("Defensa: "+ Pilotos.get(i).getdefensa());
+				sobrepaso.setText("Sobrepaso: "+ Pilotos.get(i).getsobrepaso());
+				clasificacion.setText("Clasificacion: "+Pilotos.get(i).getclasificacion());
+				cantCarrerasGanadas.setText("Cantiddad de carreras ganadas: "+ Pilotos.get(i).getcantCarrerasGanadas());
+				cantParticipaciones.setText("Cantiddad de parcitipaciones: "+ Pilotos.get(i).getcantParticipaciones());
+				cuidadoNeumaticos.setText("Cuidado de neumaticos: "+ Pilotos.get(i).getcuidadoNeumaticos());
+				largada.setText("Largada: "+ Pilotos.get(i).getlargada());
+				}
+			}
+	 		
+	 	});
+	 	
+	 	
 	//labels//
 		titulo = new JLabel();
 		titulo.setBounds(425, 0,300, 20);
@@ -52,63 +99,63 @@ public class SelcetionView extends JFrame{
 		
 		nombre = new JLabel();
 		nombre.setBounds(75, 100,200,15);
-		nombre.setText("Nombre: "+ Pilotos.get(0).getnombre());
+		nombre.setText("Nombre: "+ Pilotos.get(i).getnombre());
 		panel.add(nombre);
 		
 		nomabv = new JLabel();
 		nomabv.setBounds(220, 100, 100, 15);
-		nomabv.setText(Pilotos.get(0).getnombreAbrev());
+		nomabv.setText(Pilotos.get(i).getnombreAbrev());
 		nomabv.setHorizontalAlignment(SwingConstants.CENTER);
 		panel.add(nomabv);
 		
 		presupuesto = new JLabel();
 		presupuesto.setBounds(75, 115, 200, 15);
-		presupuesto.setText("Presupuesto: " + Pilotos.get(0).getpresupuesto());
+		presupuesto.setText("Presupuesto: " + Pilotos.get(i).getpresupuesto());
 		panel.add(presupuesto);
 		
 		defensa = new JLabel();
 		defensa.setBounds(75, 130, 200, 15);
-		defensa.setText("Defensa: "+ Pilotos.get(0).getdefensa());
+		defensa.setText("Defensa: "+ Pilotos.get(i).getdefensa());
 		panel.add(defensa);
 		
 		sobrepaso = new JLabel();
 		sobrepaso.setBounds(75, 145, 200, 15);
-		sobrepaso.setText("Sobrepaso: "+ Pilotos.get(0).getsobrepaso());
+		sobrepaso.setText("Sobrepaso: "+ Pilotos.get(i).getsobrepaso());
 		panel.add(sobrepaso);
 		
 		clasificacion = new JLabel();
 		clasificacion.setBounds(75, 160, 200, 15);
-		clasificacion.setText("Clasificacion: "+Pilotos.get(0).getclasificacion());
+		clasificacion.setText("Clasificacion: "+Pilotos.get(i).getclasificacion());
 		panel.add(clasificacion);
 		
 		cantCarrerasGanadas = new JLabel();
 		cantCarrerasGanadas.setBounds(75, 175, 220, 15);
-		cantCarrerasGanadas.setText("Cantiddad de carreras ganadas: "+ Pilotos.get(0).getcantCarrerasGanadas());
+		cantCarrerasGanadas.setText("Cantiddad de carreras ganadas: "+ Pilotos.get(i).getcantCarrerasGanadas());
 		panel.add(cantCarrerasGanadas);
 		
 		cantPolePosition = new JLabel();
 		cantPolePosition.setBounds(75, 190, 200, 15);
-		cantPolePosition.setText("Cantiddad de pole: "+ Pilotos.get(0).getcantPolePosition());
+		cantPolePosition.setText("Cantiddad de pole: "+ Pilotos.get(i).getcantPolePosition());
 		panel.add(cantPolePosition);
 		
 		cantCampeonatos = new JLabel();
 		cantCampeonatos.setBounds(75, 205, 250, 15);
-		cantCampeonatos.setText("Cantiddad de camponeatos ganados: "+ Pilotos.get(0).getcantCampeonatos());
+		cantCampeonatos.setText("Cantiddad de camponeatos ganados: "+ Pilotos.get(i).getcantCampeonatos());
 		panel.add(cantCampeonatos);
 		
 		cantParticipaciones = new JLabel();
 		cantParticipaciones.setBounds(75, 220, 200, 15);
-		cantParticipaciones.setText("Cantiddad de parcitipaciones: "+ Pilotos.get(0).getcantParticipaciones());
+		cantParticipaciones.setText("Cantiddad de parcitipaciones: "+ Pilotos.get(i).getcantParticipaciones());
 		panel.add(cantParticipaciones);
 		
 		cuidadoNeumaticos = new JLabel();
 		cuidadoNeumaticos.setBounds(75, 235, 200, 15);
-		cuidadoNeumaticos.setText("Cuidado de neumaticos: "+ Pilotos.get(0).getcuidadoNeumaticos());
+		cuidadoNeumaticos.setText("Cuidado de neumaticos: "+ Pilotos.get(i).getcuidadoNeumaticos());
 		panel.add(cuidadoNeumaticos);
 
 		largada = new JLabel();
 		largada.setBounds(75, 250, 200, 15);
-		largada.setText("Largada: "+ Pilotos.get(0).getlargada());
+		largada.setText("Largada: "+ Pilotos.get(i).getlargada());
 		panel.add(largada);
 		
 		imagen = new JLabel();
@@ -116,6 +163,8 @@ public class SelcetionView extends JFrame{
 		imagen.setOpaque(true);
 		imagen.setBackground(Color.black);
 		panel.add(imagen);
-	}
+
+		
+ }
 
 }
